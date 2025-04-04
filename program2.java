7dd9991ffbc870318f5e9cc8660e6a48d98906f3
veeres-javali18 <veeresjavali038@gmail.com>
2025-04-04 09:00:52 +0530

    Save this

---
+++ program2.java
@@ -0,0 +1,18 @@
+package javacode;
+
+public class program2 {
+    public static void main(String[] args)
+    {
+       int a=10;
+       int b=20;
+       System.out.println("Addition is"+(a+b)); 
+       System.out.println("substraction is"+(a-b)); 
+       System.out.println("Multiplication is"+(a*b)); 
+       System.out.println("Division is"+(a/b)); 
+       System.out.println("Modulous is"+(a%b)); 
+       System.out.println("Arithmetic operations");
+       
+    }
+    
+    
+}
