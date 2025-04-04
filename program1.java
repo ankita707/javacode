8b6bb3655e63d8ae4e80fecfe46f55376759f1b1
veeres-javali18 <veeresjavali038@gmail.com>
2025-04-04 08:30:30 +0530

    update this file

--- program1.java
+++ program1.java
@@ -3,7 +3,13 @@ package javacode;
 public class program1 {
     public static void main(String[] args)
     {
-        
+       int a=10;
+       int b=20;
+       System.out.println("Addition is"+(a+b)); 
+       System.out.println("substraction is"+(a-b)); 
+       System.out.println("Multiplication is"+(a*b)); 
+       System.out.println("Division is"+(a/b)); 
+       System.out.println("Modulous is"+(a%b)); 
     }
     
 }
